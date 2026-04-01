0eec80dd4ba0bbe0df3f7cdd67bceecae9eaf97f
AdrianoCelentano <adrian.schaefer87@gmail.com>
2026-04-01 13:35:46 +0200

    activate tower endpoint

--- src/main/kotlin/com/example/demo/model/GameMapDataEggenstein.kt
+++ src/main/kotlin/com/example/demo/model/GameMapDataEggenstein.kt
@@ -30,7 +30,6 @@ val eggensteinTowers = listOf(
     Tower(position = LatLng(49.08981433407536, 8.3993526430239)),
     Tower(position = LatLng(49.089507654775105, 8.40233032721529)),
     Tower(position = LatLng(49.086840043604056, 8.39901688041478)),
-    Tower(position = LatLng(49.08664908112527, 8.400969605062548)),
     Tower(position = LatLng(49.08789321741393, 8.402259640350213)),
     Tower(position = LatLng(49.08664908112527, 8.400969605062548)),
     Tower(position = LatLng(49.08871490893885, 8.401685309571459)),
